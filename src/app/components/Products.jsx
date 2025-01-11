6e66a46864159b7cb0c73f0e96520d68b7ab3577
chandan <chandansamantaray784@gmail.com>
2025-01-11 16:18:18 +0530

    jhf

--- src/app/components/Products.jsx
+++ src/app/components/Products.jsx
@@ -167,7 +167,9 @@ export default function Hotels() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h2 className="text-2xl font-bold text-center mb-6">Available Hotels</h2>
+      <h2 className="text-2xl font-bold text-center mb-6">
+        Available Cottages
+      </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {hotels.map((hotel) => (
           <div
